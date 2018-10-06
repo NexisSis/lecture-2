b66b2d92d03851fd5df7f9d9ee42473810af0335
Nexis Sis <nexis.sis@gmail.com>
2018-10-07 01:10:12 +0300

    add pointer move

--- src/main.js
+++ src/main.js
@@ -1,1 +1,17 @@
-console.log('hello');
+$( document ).ready(function() {
+    if($('.viewport').width()){
+        var image = $('#image')[0];
+        var progress;
+        var width = $('.viewport').width();
+        image.addEventListener("pointermove", function(e) {
+            if(e.pageX<width){
+                $('#image').css("right",e.pageX);
+                progress = e.pageX*100/width;
+                console.log(progress);
+                $(".move-progress").css("width", progress+"%");
+                console.log(e);
+            }
+        });
+    }
+
+});
